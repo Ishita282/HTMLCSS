d755b1d2e5ac1ba4264f804029e95a812aaf08e5
Ishita282 <littleminiishita@gmail.com>
2026-01-27 01:17:16 +0530

    separate files

---
+++ script.js
@@ -0,0 +1,18 @@
+const greetButton= document.getElementById("button");
+const nameInput= document.getElementById("text");
+const greetingName= document.getElementById("greet");
+const boxes= document.querySelectorAll(".Box");
+
+greetButton.addEventListener("click", () =>{
+    const name= nameInput.value;
+    greetingName.textContent= `Hello, ${name}`
+
+})
+
+boxes.forEach((box)=> {
+    box.addEventListener("click", ()=>{
+        const color= box.textContent;
+        box.style.backgroundColor = color;
+        box.style.color = "white";
+    })
+})
